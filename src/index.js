1cea61cd5153ec5c3f3df87238ad4aaa675f3957
Валерия <sp.ether@yandex.ru>
2023-08-16 01:11:13 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];
+    const exprArrOfLetters = expr.match(/.{1,10}/g);
+    
+    exprArrOfLetters.forEach(letter => {
+        const symbols = letter.match(/.{1,2}/g);
+        const arr2 = [];
+        for (let i = 0; i < symbols.length; i++) {
+            if (symbols[i] === '00') arr2.push('');
+            if (symbols[i] === '10') arr2.push('.');
+            if (symbols[i] === '11') arr2.push('-');
+            if (symbols[i] === '**') arr2.push(' ');
+        }
+        arr.push(arr2.join(''));
+    })
+    const arr3 = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '     ') arr3.push(' ');
+        if (MORSE_TABLE[arr[i]]) arr3.push(MORSE_TABLE[arr[i]]);
+    }
+    return arr3.join('');
 }
 
 module.exports = {
